bdbdd7d243d472ee004fe68a489262aa9046debb
wobeo <mywobeo@gmail.com>
2019-12-17 10:07:00 +0100

    Dev Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LeavesComponent } from './leaves/leaves.component';
 import { Page404Component } from './page404/page404.component';
 import { ProfileComponent } from './profile/profile.component';
-import { ApplyComponent } from './leaves/apply/apply.component';
-import { BalanceComponent } from './leaves/balance/balance.component';
-import { Page404leavesComponent } from './leaves/page404leaves/page404leaves.component';
-import { CasualComponent } from './leaves/balance/casual/casual.component';
-import { EarnedComponent } from './leaves/balance/earned/earned.component';
-import { Page404balanceComponent } from './leaves/balance/page404balance/page404balance.component';
+// import { ApplyComponent } from './leaves/apply/apply.component';
+// import { BalanceComponent } from './leaves/balance/balance.component';
+// import { Page404leavesComponent } from './leaves/page404leaves/page404leaves.component';
+// import { CasualComponent } from './leaves/balance/casual/casual.component';
+// import { EarnedComponent } from './leaves/balance/earned/earned.component';
+// import { Page404balanceComponent } from './leaves/balance/page404balance/page404balance.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +23,12 @@ import { Page404balanceComponent } from './leaves/balance/page404balance/page404
     LeavesComponent,
     Page404Component,
     ProfileComponent,
-    ApplyComponent,
-    BalanceComponent,
-    Page404leavesComponent,
-    CasualComponent,
-    EarnedComponent,
-    Page404balanceComponent
+    // ApplyComponent,
+    // BalanceComponent,
+    // Page404leavesComponent,
+    // CasualComponent,
+    // EarnedComponent,
+    // Page404balanceComponent
   ],
   imports: [
     BrowserModule,
